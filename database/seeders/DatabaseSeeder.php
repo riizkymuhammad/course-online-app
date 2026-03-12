718f601be8cbbfd02338f499454e61b80332e493
Muhammad Rizky <mrizky98@gmail.com>
2026-03-12 15:21:50 +0700

    seeder role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,11 +18,36 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $superAdminRole = Role::findOrCreate('superadmin');
+        $fasilitatorRole = Role::findOrCreate('fasilitator');
+        $studentRole = Role::findOrCreate('student');
+
+        $superAdmin = User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@example.com',
+            'password' => '12345678',
+            'role' => 'superadmin',
         ]);
+
+        $fasilitator = User::factory()->create([
+            'name' => 'Fasilitator',
+            'email' => 'fasilitator@example.com',
+            'password' => '12345678',
+            'role' => 'fasilitator',
+        ]);
+
+        $student = User::factory()->create([
+            'name' => 'Student',
+            'email' => 'student@example.com',
+            'password' => '12345678',
+            'role' => 'student',
+        ]);
+
+        $superAdmin->assignRole($superAdminRole);
+        $fasilitator->assignRole($fasilitatorRole);
+        $student->assignRole($studentRole);
 
         $this->call(CourseContentSeeder::class);
     }
